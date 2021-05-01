43e2d0f7a50241c0abbe34c3c17dd9bf7b074ade
김광훈 <kk03187693@gmail.com>
2021-05-01 20:06:16 +0900

    add transaction class

---
+++ chapter3/src/main/java/com/study/chapter2/database/TransactionSQLiteJdbcOld.java
@@ -0,0 +1,21 @@
+package com.study.chapter2.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class TransactionSQLiteJdbcOld extends Transaction{
+
+    @Override
+    protected Connection createConnection() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC"); // https://kyun2.tistory.com/23
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // DriverManager 에 JDBC 객체 등록
+        return DriverManager.getConnection("jdbc:sqlite:C:\\public\\sqlite\\code5.db");
+    }
+}
---
+++ chapter3/src/main/java/com/study/chapter2/database/TransactionSQLitePool.java
@@ -0,0 +1,4 @@
+package com.study.chapter2.database;
+
+public class TransactionSQLitePool {
+}
