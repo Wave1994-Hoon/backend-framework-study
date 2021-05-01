9fd170eaa9ce77c1b55987107f4ac95b0e7e39f7
김광훈 <kk03187693@gmail.com>
2021-05-01 19:06:33 +0900

    add chpater 3 inital setting

---
+++ chapter3/src/main/java/com/study/chapter2/box/BoxContext.java
@@ -0,0 +1,30 @@
+package com.study.chapter2.box;
+
+public class BoxContext {
+
+    private static ThreadLocal<Box> threadLocal = new ThreadLocal<Box>();
+
+    /* 개발자가 직접 호출하면 안되기 때문에 default 접근 제어 사용 */
+    static void setThreadLocal(Box box) {
+        threadLocal.set(box);
+    }
+
+    static void removeThreadLocal() {
+        threadLocal.remove();
+    }
+
+    public static Box getThread() {
+        return getBox();
+    }
+
+    private static Box getBox() {
+        Box box = threadLocal.get();
+
+        if (box == null) {
+            BoxLocal boxLocal = new BoxLocal();
+            setThreadLocal(boxLocal);
+        }
+
+        return box;
+    }
+}
